92461a52e83e68fb54af59de716ace52f7fc7810
Walison Guinzani <w.guinzani@gmail.com>
2021-07-07 22:14:43 -0300

    Initial commit

---
+++ conexao.php
@@ -0,0 +1,37 @@
+﻿<?php
+	$host = "sql1.hostinger.com.br/phpmyadmin";
+	$database = "u808352183_work";
+	$user = "u808352183_jobs";
+	$pass = "w4regt5o612319";
+
+	$conexao = mysql_connect($host, $user, $pass) or print(trigger_error(mysql_error()));
+
+	mysql_select_db($database, $conexao);
+	
+	mysql_set_charset('utf8');
+
+?>
+
+<?php
+		if(!empty($_POST)){
+			
+		$uploaddir = 'uploads';
+
+		$nome= $_POST['nome'];
+		$tel= $_POST['tel'];
+		$cv= $uploaddir . $_FILES['cv'];
+		
+		$sql = mysql_query("INSERT INTO curriculum(nome, tel) VALUES('$nome, $tel')");
+	
+		if(move_uploaded_file($_FILES['cv']['tmp_name'], $cv)){			
+			echo "<script type=\"text/javascript\">alert(\"Seu curriculum foi enviado com sucesso!\"); history.go(-1);</script>\n";		
+		}
+		else{
+			echo "<script type=\"text/javascript\">alert(\"Ocorreu um erro ao tentar enviar este curriculum!\"); history.go(-1); <\script>\n";
+		}
+	}
+	else{
+		header("Lacation:index.html");
+	}
+
+?>
---
+++ cv.php
@@ -0,0 +1,34 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mensagem Enviada!</title>
+</head>
+<body>
+<?php
+	if(!empty($_POST)){
+
+		$cabe= "Form: ".$_POST['nome']."<".$_POST['email'].">\n";
+	
+		$atraves = "Contato através do Site www.guinzani.com.br\n";
+		$atraves.= "Nome: ".$_POST['nome']."\n";
+		$atraves.= "E-mail: ".$_POST['email']."\n";
+		$atraves.= "Telefone: ".$_POST['tel']."\n";
+		$atraves.= "Assunto: ".$_POST['ass']."\n";
+		$atraves.= "Mensagem: ".$_POST['msg']."\n";
+	
+		if(mail("guinzaniwebdesign@gmail.com", "Contato através do Site www.guinzani.com.br", $atraves, $cabe)){
+			echo "<script type=\"text/javascript\">alert(\"Sua mensagem foi enviada com sucesso!\"); history.go(-1);</script>\n";		
+		}
+		else{
+			echo "<script type=\"text/javascript\">alert(\"Ocorreu um erro ao tentar enviar está mensagem!\"); history.go(-1); <\script>\n";
+		}
+	}
+	else{
+		header("Lacation:index.html");
+	}
+	
+?>
+
+</body>
+</html>
